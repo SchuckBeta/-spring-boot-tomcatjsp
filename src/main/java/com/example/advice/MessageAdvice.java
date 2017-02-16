dab5cde7fb359d957492b8020a5639fd650ca37a
陈浩 <chenhao@wanfin.com>
2017-02-16 18:09:17 +0800

    init spring-boot-tomcatjs

---
+++ src/main/java/com/example/advice/MessageAdvice.java
@@ -0,0 +1,9 @@
+package com.example.advice;
+
+//@ControllerAdvice
+public class MessageAdvice {
+//	@ModelAttribute("message")
+//	public String message(@Value("${application.message:Hello World}") String message) {
+//	        return message;
+//	}
+}
