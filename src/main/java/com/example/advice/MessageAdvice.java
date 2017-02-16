54db50ff80741a9f326f7892f74b0a1158fc095b
陈浩 <chenhao@wanfin.com>
2017-02-16 18:11:40 +0800

    del MessageAdvice.java

--- src/main/java/com/example/advice/MessageAdvice.java
+++
@@ -1,9 +0,0 @@
-package com.example.advice;
-
-//@ControllerAdvice
-public class MessageAdvice {
-//	@ModelAttribute("message")
-//	public String message(@Value("${application.message:Hello World}") String message) {
-//	        return message;
-//	}
-}
